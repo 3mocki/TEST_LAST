e4dc123c6a04492a61a1750ba4c8b2a3ecb30c11
3mocki <starleesm@gmail.com>
2019-05-28 12:07:04 -0700

    05282019

--- multiThread.py
+++ multiThread.py
@@ -19,4 +19,4 @@ if __name__=="__main__":
     last = time.time()
     gap = last - first
     print("It took " + str(gap) + " sec")
-print("Result : {sum(result)}")
+    print("Result : {sum(result)}")
